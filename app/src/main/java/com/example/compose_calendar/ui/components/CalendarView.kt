9205b5674984670bccad1a6238ba60e5c378fa97
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-09-20 10:31:45 +0545

    add month row

--- app/src/main/java/com/example/compose_calendar/ui/components/CalendarView.kt
+++ app/src/main/java/com/example/compose_calendar/ui/components/CalendarView.kt
@@ -9,8 +9,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,9 +24,17 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun CalendarView(modifier: Modifier = Modifier) {
     Column (modifier = modifier){
+        Row (verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp),
+            ){
+            Icon(imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft, "arrow left")
+            Text("September 2024")
+            Icon(imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight, "arrow right")
+        }
         Row(
             modifier= Modifier.fillMaxWidth().padding(vertical = 12.dp),
-            horizontalArrangement = Arrangement.SpaceAround) {
+            horizontalArrangement = Arrangement.SpaceBetween) {
             Text("M")
             Text("T")
             Text("W")
