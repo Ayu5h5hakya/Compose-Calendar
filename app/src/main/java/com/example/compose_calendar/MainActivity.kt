6db1f2f46fe758e6ed1f24cc929276cfd114ca51
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-09-20 10:12:33 +0545

    initial commit

---
+++ app/src/main/java/com/example/compose_calendar/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.compose_calendar
+
+import android.os.Bundle
+import android.widget.CalendarView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_calendar.ui.theme.ComposecalendarTheme
+import java.util.Calendar
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposecalendarTheme {
+                CalendarView()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun CalendarView() {
+    val pageState = rememberPagerState(pageCount = {10})
+    HorizontalPager(state = pageState) {page ->
+        Text("page $page",
+            modifier = Modifier.fillMaxSize(),
+            )
+    }
+}
